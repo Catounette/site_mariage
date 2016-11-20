d2d5c8e5aa806b7e7d0186ab9bdff99cdfa34135
Marion <marion.leclercq@gmail.com>
2016-11-20 20:59:34 +0100

    commit5

--- blog/models.py
+++ blog/models.py
@@ -14,10 +14,5 @@ class Guest(models.Model):
         self.save()
 
     def __str__(self):
-        """ 
-        Cette méthode que nous définirons dans tous les modèles
-        nous permettra de reconnaître facilement les différents objets que 
-        nous traiterons plus tard et dans l'administration
-        """
         return self.nom
 
